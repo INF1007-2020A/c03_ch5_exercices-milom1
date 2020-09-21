d9428f45b1b48a1b18bda1bcd541e5d7c6fafcca
milom1 <71018856+milom1@users.noreply.github.com>
2020-09-21 14:58:56 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -23,7 +23,19 @@ def use_prefixes() -> List[str]:
 
 
 def prime_integer_summation() -> int:
-    return 0
+nombrePremier = 0
+nombre = 2
+sommes = 0
+while(nombrePremier < 100):
+    for i in range(1, nombre+1):
+        if (nombre % i == 0 and i != 1 and i != nombre):
+            break
+        elif(i == nombre):
+            nombrePremier += 1
+            sommes += nombre
+    nombre += 1
+
+return sommes 
 
 
 def factorial(number: int) -> int:
